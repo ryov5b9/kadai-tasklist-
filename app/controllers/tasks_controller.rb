8567d1ba2cd0a6a997173ef055d0cc08389bfbb7
ryo.v5b9 <hryov5b9@gamil.com>
2019-05-24 04:32:29 +0000

    comp

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,17 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy, :edit, :update, :show]
+  
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    set_task
   end
 
   def new
-    @task = Task.new 
+    @task = current_user.tasks.build
   end
 
   def create
@@ -27,11 +26,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end
 
   def update
-    set_task
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -42,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
@@ -50,11 +46,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   
   def task_params
     params.require(:task).permit(:content, :status)
