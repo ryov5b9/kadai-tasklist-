8bce769ba548127b514db26a7c02d0f78ea220d1
ryo.v5b9 <hryov5b9@gamil.com>
2019-05-26 07:43:02 +0000

    C

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :edit, :update, :show]
+  before_action :correct_user, only: [:destroy, :edit, :update]
   
   
   def index
